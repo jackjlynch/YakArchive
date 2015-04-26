dc06c7f3618bbdc6fab5c110e196bb329fdc388a
Jack Lynch <jacklynch@wharnal.com>
2015-04-26 03:17:10 -0400

    improved admin page

--- yaks/admin.py
+++ yaks/admin.py
@@ -3,5 +3,9 @@ from yaks import models
 
 # Register your models here.
 admin.site.register(models.YakLocation)
-admin.site.register(models.Yak)
 admin.site.register(models.Comment)
+
+class YakAdmin(admin.ModelAdmin):
+    list_display = ('message', 'time', 'poster_id')
+
+admin.site.register(models.Yak, YakAdmin)
