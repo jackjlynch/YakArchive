d0f98e0f83f6c62508018279b1f855b07ab42a5d
Jack Lynch <jacklynch@wharnal.com>
2015-04-26 03:11:12 -0400

    improved display of yaks and comments

--- yaks/views.py
+++ yaks/views.py
@@ -1,8 +1,16 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from yaks.models import Yak
+from yaks.models import Yak, Comment, YakLocation
 
 # Create your views here.
 
 def index(request):
-    return HttpResponse('<br /><br / >'.join([y.message for y in Yak.objects.order_by('-time')]))
+    output = ""
+    for location in YakLocation.objects.all():
+        output += "<h1>" + location.name + "</h1>"
+        for yak in Yak.objects.filter(yaklocation=location).order_by("-time"):
+            output += "<p><h2>" + yak.message +"</h2>"
+            for comment in Comment.objects.filter(message_id=yak.message_id).order_by("-time"):
+                output += comment.comment + "<br/>"
+            output += "</p>"
+    return HttpResponse(output)
