18d9bee11a332641e7408c13df3b995ff3bd7d65
Jack Lynch <jacklynch@wharnal.com>
2015-04-26 03:02:15 -0400

    added views and url for yaks

--- yaks/views.py
+++ yaks/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from yaks.models import Yak
 
 # Create your views here.
+
+def index(request):
+    return HttpResponse('<br /><br / >'.join([y.message for y in Yak.objects.order_by('-time')]))
