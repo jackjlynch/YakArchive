e8c7bfd6dea4198855cc6a3df5147f9e9017574c
Jack Lynch <jacklynch@wharnal.com>
2015-04-27 15:50:52 -0400

    fixed view

--- yaks/views.py
+++ yaks/views.py
@@ -9,8 +9,8 @@ def index(request):
     for location in YakLocation.objects.all():
         output += "<h1>" + location.name + "</h1>"
         for yak in Yak.objects.filter(yaklocation=location).order_by("-time"):
-            output += "<p><h2>" + yak.message + " (" + yak.likes + ")</h2>"
+            output += "<p><h2>" + yak.message + " (" + str(yak.likes) + ")</h2>"
             for comment in Comment.objects.filter(message_id=yak.message_id).order_by("-time"):
-                output += comment.comment + " (" + comment.likes + ")<br/>"
+                output += comment.comment + " (" + str(comment.likes) + ")<br/>"
             output += "</p>"
     return HttpResponse(output)
